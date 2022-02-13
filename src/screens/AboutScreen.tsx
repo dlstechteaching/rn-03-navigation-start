56a14c6d7e3cb3f1166eb052fed7567498eac49e
dls.tech.teaching <dls.tech.teaching@gmail.com>
2022-02-13 19:09:00 +0100

    adding initial screens

---
+++ src/screens/AboutScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export const AboutScreen: React.VFC = () => {
+
+    return (
+        <View style={{ padding: 20, display: 'flex', alignItems: 'center'}}>
+            <Text>AboutScreen</Text>
+        </View>
+    )
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const HomeScreen: React.VFC = () => {
+    return (
+        <View style={{ padding: 20, display: 'flex', alignItems: 'center'}}>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
